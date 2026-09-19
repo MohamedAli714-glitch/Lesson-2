0643663a9b789bfb2b58f8fd051941c3981c3ddb
MohamedAli714-glitch <nehal.shahtout2010@gmail.com>
2026-09-19 14:08:11 +0300

    Grandpa's birthday is coming up in 10 days!

---
+++ homework.py
@@ -0,0 +1,15 @@
+Mybirthday=("2015-09-14")
+
+MOMSbirthday=("1985-08-07")
+
+Dadsbirthday=("1986-02-15")
+
+grandmasbirthday=("1953-06-05")
+
+grandpasbirthday=("1953-09-29")
+
+print("My birthday is on", Mybirthday)
+print("My mom's birthday is on", MOMSbirthday)
+print("My dad's birthday is on", Dadsbirthday)
+print("My grandma's birthday is on", grandmasbirthday)
+print("My grandpa's birthday is on", grandpasbirthday)
