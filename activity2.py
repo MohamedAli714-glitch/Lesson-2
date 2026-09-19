cc73419c6a14f922ce5261c89ad5f15f9d260888
MohamedAli714-glitch <nehal.shahtout2010@gmail.com>
2026-09-19 13:33:25 +0300

    bye

---
+++ activity2.py
@@ -0,0 +1,10 @@
+variable="Mohamed"
+variable2=11
+variable3="coding"
+print("My name is", variable)
+print("I am", variable2, "years old")
+print("I learn", variable3)
+name=input("Enter your name:  ")
+print(name)
+hobby=input("Enter your hobby:  ")
+print(hobby)
